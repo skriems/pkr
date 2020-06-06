a9349bfbc4656dc47eddadbc8cf05723cf560695
Sebastian Kriems <bastoberto@gmx.de>
2020-06-06 21:13:44 +0200

    `Deck` ownes the cards now

--- src/deck.rs
+++ src/deck.rs
@@ -1,45 +1,87 @@
 use crate::card::*;
-use crate::error::{Error, Result};
+use crate::error::Result;
 use crate::holding::*;
 
 use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
 
 /// A Deck of Cards
-#[derive(Debug)]
-pub struct Deck<'a> {
-    cards: &'a mut [Card],
+pub struct Deck {
+    pub cards: [Card; 52],
     rng: ThreadRng,
-    idx: u8,
 }
 
-impl<'a> Deck<'a> {
-    pub fn new(cards: &'a mut [Card]) -> Result<Self> {
-        if cards.len() != 52 {
-            return Err(Error::InvalidDeck);
-        }
+impl Deck {
+    pub fn new() -> Self {
+        let cards: [Card; 52] = [
+            Card::new(Rank::Ace, Suit::Clubs),
+            Card::new(Rank::King, Suit::Clubs),
+            Card::new(Rank::Queen, Suit::Clubs),
+            Card::new(Rank::Jack, Suit::Clubs),
+            Card::new(Rank::Ten, Suit::Clubs),
+            Card::new(Rank::Nine, Suit::Clubs),
+            Card::new(Rank::Eight, Suit::Clubs),
+            Card::new(Rank::Seven, Suit::Clubs),
+            Card::new(Rank::Six, Suit::Clubs),
+            Card::new(Rank::Five, Suit::Clubs),
+            Card::new(Rank::Four, Suit::Clubs),
+            Card::new(Rank::Three, Suit::Clubs),
+            Card::new(Rank::Two, Suit::Clubs),
+            Card::new(Rank::Ace, Suit::Spades),
+            Card::new(Rank::King, Suit::Spades),
+            Card::new(Rank::Queen, Suit::Spades),
+            Card::new(Rank::Jack, Suit::Spades),
+            Card::new(Rank::Ten, Suit::Spades),
+            Card::new(Rank::Nine, Suit::Spades),
+            Card::new(Rank::Eight, Suit::Spades),
+            Card::new(Rank::Seven, Suit::Spades),
+            Card::new(Rank::Six, Suit::Spades),
+            Card::new(Rank::Five, Suit::Spades),
+            Card::new(Rank::Four, Suit::Spades),
+            Card::new(Rank::Three, Suit::Spades),
+            Card::new(Rank::Two, Suit::Spades),
+            Card::new(Rank::Ace, Suit::Hearts),
+            Card::new(Rank::King, Suit::Hearts),
+            Card::new(Rank::Queen, Suit::Hearts),
+            Card::new(Rank::Jack, Suit::Hearts),
+            Card::new(Rank::Ten, Suit::Hearts),
+            Card::new(Rank::Nine, Suit::Hearts),
+            Card::new(Rank::Eight, Suit::Hearts),
+            Card::new(Rank::Seven, Suit::Hearts),
+            Card::new(Rank::Six, Suit::Hearts),
+            Card::new(Rank::Five, Suit::Hearts),
+            Card::new(Rank::Four, Suit::Hearts),
+            Card::new(Rank::Three, Suit::Hearts),
+            Card::new(Rank::Two, Suit::Hearts),
+            Card::new(Rank::Ace, Suit::Diamonds),
+            Card::new(Rank::King, Suit::Diamonds),
+            Card::new(Rank::Queen, Suit::Diamonds),
+            Card::new(Rank::Jack, Suit::Diamonds),
+            Card::new(Rank::Ten, Suit::Diamonds),
+            Card::new(Rank::Nine, Suit::Diamonds),
+            Card::new(Rank::Eight, Suit::Diamonds),
+            Card::new(Rank::Seven, Suit::Diamonds),
+            Card::new(Rank::Six, Suit::Diamonds),
+            Card::new(Rank::Five, Suit::Diamonds),
+            Card::new(Rank::Four, Suit::Diamonds),
+            Card::new(Rank::Three, Suit::Diamonds),
+            Card::new(Rank::Two, Suit::Diamonds),
+        ];
 
-        let mut rng = ThreadRng::default();
-        let idx = 0;
-        Ok(Deck { cards, rng, idx })
+        let rng = ThreadRng::default();
+        Deck { cards, rng }
     }
 
-    pub fn shuffled(cards: &'a mut [Card]) -> Result<Self> {
-        if cards.len() != 52 {
-            return Err(Error::InvalidDeck);
-        }
-
-        let mut rng = ThreadRng::default();
-        let idx = 0;
-
+    pub fn shuffle(&mut self) {
+        let mut rng = self.rng;
+        let mut cards = self.cards;
         cards.shuffle(&mut rng);
-        Ok(Deck { cards, rng, idx })
+        self.cards = cards;
     }
 
     pub fn get_holding(&mut self) -> Result<Holding> {
         Holding::new(&self.cards[..2])
     }
-
 }
 
 #[cfg(test)]
@@ -48,7 +90,6 @@ mod tests {
 
     #[test]
     fn len() {
-        // assert_eq!(Deck::new().cards.len(), 52);
-        assert!(true);
+        assert_eq!(Deck::new().cards.len(), 52);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,34 @@
 use pkr::prelude::*;
 
-use rand;
-use rand::seq::SliceRandom;
-use std::mem;
+// use std::mem;
 
 fn main() {
-    let mut cards: [Card; 52] = [
-        Card::new(Rank::Ace, Suit::Clubs),
-        Card::new(Rank::King, Suit::Clubs),
-        Card::new(Rank::Queen, Suit::Clubs),
-        Card::new(Rank::Jack, Suit::Clubs),
-        Card::new(Rank::Ten, Suit::Clubs),
-        Card::new(Rank::Nine, Suit::Clubs),
-        Card::new(Rank::Eight, Suit::Clubs),
-        Card::new(Rank::Seven, Suit::Clubs),
-        Card::new(Rank::Six, Suit::Clubs),
-        Card::new(Rank::Five, Suit::Clubs),
-        Card::new(Rank::Four, Suit::Clubs),
-        Card::new(Rank::Three, Suit::Clubs),
-        Card::new(Rank::Two, Suit::Clubs),
-        Card::new(Rank::Ace, Suit::Spades),
-        Card::new(Rank::King, Suit::Spades),
-        Card::new(Rank::Queen, Suit::Spades),
-        Card::new(Rank::Jack, Suit::Spades),
-        Card::new(Rank::Ten, Suit::Spades),
-        Card::new(Rank::Nine, Suit::Spades),
-        Card::new(Rank::Eight, Suit::Spades),
-        Card::new(Rank::Seven, Suit::Spades),
-        Card::new(Rank::Six, Suit::Spades),
-        Card::new(Rank::Five, Suit::Spades),
-        Card::new(Rank::Four, Suit::Spades),
-        Card::new(Rank::Three, Suit::Spades),
-        Card::new(Rank::Two, Suit::Spades),
-        Card::new(Rank::Ace, Suit::Hearts),
-        Card::new(Rank::King, Suit::Hearts),
-        Card::new(Rank::Queen, Suit::Hearts),
-        Card::new(Rank::Jack, Suit::Hearts),
-        Card::new(Rank::Ten, Suit::Hearts),
-        Card::new(Rank::Nine, Suit::Hearts),
-        Card::new(Rank::Eight, Suit::Hearts),
-        Card::new(Rank::Seven, Suit::Hearts),
-        Card::new(Rank::Six, Suit::Hearts),
-        Card::new(Rank::Five, Suit::Hearts),
-        Card::new(Rank::Four, Suit::Hearts),
-        Card::new(Rank::Three, Suit::Hearts),
-        Card::new(Rank::Two, Suit::Hearts),
-        Card::new(Rank::Ace, Suit::Diamonds),
-        Card::new(Rank::King, Suit::Diamonds),
-        Card::new(Rank::Queen, Suit::Diamonds),
-        Card::new(Rank::Jack, Suit::Diamonds),
-        Card::new(Rank::Ten, Suit::Diamonds),
-        Card::new(Rank::Nine, Suit::Diamonds),
-        Card::new(Rank::Eight, Suit::Diamonds),
-        Card::new(Rank::Seven, Suit::Diamonds),
-        Card::new(Rank::Six, Suit::Diamonds),
-        Card::new(Rank::Five, Suit::Diamonds),
-        Card::new(Rank::Four, Suit::Diamonds),
-        Card::new(Rank::Three, Suit::Diamonds),
-        Card::new(Rank::Two, Suit::Diamonds),
-    ];
+    let mut count = 0;
 
-    let mut rng = rand::thread_rng();
+    for _ in 0..1_000_000 {
+        let mut deck = Deck::new();
+        deck.shuffle();
+        let holding = Holding::new(&deck.cards[..2]).unwrap();
+        let other = Holding::new(&deck.cards[2..4]).unwrap();
 
-    for _ in 0..100 {
-        cards.shuffle(&mut rng);
-        let first = Holding::new(&cards[..2]).unwrap();
-        let second = Holding::new(&cards[2..4]).unwrap();
-        // let rest = &cards[4..9];
+        if holding.beats(&other) {
+            count += 1;
+        };
+        // if !(first.is_pocket_pair() || second.is_pocket_pair()) {
+        //     continue;
+        // }
 
-        println!(
-            "{}{} beats {}{} :: {}",
-            first.high_card(),
-            first.low_card(),
-            second.high_card(),
-            second.low_card(),
-            first.beats(&second)
-        );
+        // println!(
+        //     "{}{} beats {}{} :: {}",
+        //     first.high_card(),
+        //     first.low_card(),
+        //     second.high_card(),
+        //     second.low_card(),
+        //     first.beats(&second)
+        // );
     }
+
+    println!("{} beats!", count);
 
     // if let Ok(deck) = Deck::new(&mut cards) {
     //     let smb = &deck.get_holding();
@@ -85,10 +37,10 @@ fn main() {
     //     // println!("{:#?}", bb);
     // }
 
-    // println!("Card: {:?}", mem::size_of::<Card>());
-    // println!("Rank: {:?}", mem::size_of::<Rank>());
-    // println!("Suit: {:?}", mem::size_of::<Suit>());
-    // println!("Suit: {:?}", mem::size_of::<[Card; 52]>());
+    // println!("Card uses {:?} bytes", mem::size_of::<Card>());
+    // println!("Rank uses {:?} bytes", mem::size_of::<Rank>());
+    // println!("Suit uses {:?} bytes", mem::size_of::<Suit>());
+    // println!("Deck uses {:?} bytes", mem::size_of::<[Card; 52]>());
 
     // let first = Card::new(Rank::Ace, Suit::Clubs);
     // let second = Card::new(Rank::Ace, Suit::Spades);
