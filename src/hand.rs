1b6ef759a2d8558817bf0ec596acbf7ae4c5e5d3
Sebastian Kriems <bastoberto@gmx.de>
2020-06-08 16:14:06 +0200

    [hand] impl Default which does not shuffle; maybe usefull for pkr::math

--- src/hand.rs
+++ src/hand.rs
@@ -13,7 +13,7 @@ pub struct Hand {
 
 impl Hand {
     pub fn new() -> Self {
-        let mut cards: [Card; 52] = [
+        let mut deck: [Card; 52] = [
             Card::new(Rank::Ace, Suit::Clubs),
             Card::new(Rank::King, Suit::Clubs),
             Card::new(Rank::Queen, Suit::Clubs),
@@ -69,12 +69,68 @@ impl Hand {
         ];
 
         let mut rng = ThreadRng::default();
-        cards.shuffle(&mut rng);
-        Hand { deck: cards }
+        deck.shuffle(&mut rng);
+        Hand { deck }
     }
+}
 
-    pub fn get_holding(&mut self) -> Result<Holding> {
-        Holding::new(&self.deck[..2])
+impl Default for Hand {
+    fn default() -> Self {
+        let deck: [Card; 52] = [
+            Card::new(Rank::Ace, Suit::Clubs),
+            Card::new(Rank::King, Suit::Clubs),
+            Card::new(Rank::Queen, Suit::Clubs),
+            Card::new(Rank::Jack, Suit::Clubs),
+            Card::new(Rank::Ten, Suit::Clubs),
+            Card::new(Rank::Nine, Suit::Clubs),
+            Card::new(Rank::Eight, Suit::Clubs),
+            Card::new(Rank::Seven, Suit::Clubs),
+            Card::new(Rank::Six, Suit::Clubs),
+            Card::new(Rank::Five, Suit::Clubs),
+            Card::new(Rank::Four, Suit::Clubs),
+            Card::new(Rank::Three, Suit::Clubs),
+            Card::new(Rank::Two, Suit::Clubs),
+            Card::new(Rank::Ace, Suit::Spades),
+            Card::new(Rank::King, Suit::Spades),
+            Card::new(Rank::Queen, Suit::Spades),
+            Card::new(Rank::Jack, Suit::Spades),
+            Card::new(Rank::Ten, Suit::Spades),
+            Card::new(Rank::Nine, Suit::Spades),
+            Card::new(Rank::Eight, Suit::Spades),
+            Card::new(Rank::Seven, Suit::Spades),
+            Card::new(Rank::Six, Suit::Spades),
+            Card::new(Rank::Five, Suit::Spades),
+            Card::new(Rank::Four, Suit::Spades),
+            Card::new(Rank::Three, Suit::Spades),
+            Card::new(Rank::Two, Suit::Spades),
+            Card::new(Rank::Ace, Suit::Hearts),
+            Card::new(Rank::King, Suit::Hearts),
+            Card::new(Rank::Queen, Suit::Hearts),
+            Card::new(Rank::Jack, Suit::Hearts),
+            Card::new(Rank::Ten, Suit::Hearts),
+            Card::new(Rank::Nine, Suit::Hearts),
+            Card::new(Rank::Eight, Suit::Hearts),
+            Card::new(Rank::Seven, Suit::Hearts),
+            Card::new(Rank::Six, Suit::Hearts),
+            Card::new(Rank::Five, Suit::Hearts),
+            Card::new(Rank::Four, Suit::Hearts),
+            Card::new(Rank::Three, Suit::Hearts),
+            Card::new(Rank::Two, Suit::Hearts),
+            Card::new(Rank::Ace, Suit::Diamonds),
+            Card::new(Rank::King, Suit::Diamonds),
+            Card::new(Rank::Queen, Suit::Diamonds),
+            Card::new(Rank::Jack, Suit::Diamonds),
+            Card::new(Rank::Ten, Suit::Diamonds),
+            Card::new(Rank::Nine, Suit::Diamonds),
+            Card::new(Rank::Eight, Suit::Diamonds),
+            Card::new(Rank::Seven, Suit::Diamonds),
+            Card::new(Rank::Six, Suit::Diamonds),
+            Card::new(Rank::Five, Suit::Diamonds),
+            Card::new(Rank::Four, Suit::Diamonds),
+            Card::new(Rank::Three, Suit::Diamonds),
+            Card::new(Rank::Two, Suit::Diamonds),
+        ];
+        Hand { deck }
     }
 }
 
@@ -88,11 +144,20 @@ mod tests {
     }
 
     #[test]
+    fn default() {
+        let hand1 = Hand::default();
+        let hand2 = Hand::default();
+        let holding = Holding::new(&hand1.deck[..2]).unwrap();
+        let other = Holding::new(&hand2.deck[..2]).unwrap();
+        assert_eq!(holding, other);
+    }
+
+    #[test]
     fn shuffle() {
-        let mut hand1 = Hand::new();
-        let mut hand2 = Hand::new();
-        let holding = hand1.get_holding().unwrap();
-        let other = hand2.get_holding().unwrap();
+        let hand1 = Hand::new();
+        let hand2 = Hand::new();
+        let holding = Holding::new(&hand1.deck[..2]).unwrap();
+        let other = Holding::new(&hand2.deck[..2]).unwrap();
         assert_ne!(holding, other);
     }
 
