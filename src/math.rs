1f66490cd4c62ce28c98a29555924fb12010336f
Sebastian Kriems <bastoberto@gmx.de>
2020-06-08 10:17:40 +0200

    [math] docs init

---
+++ src/math.rs
@@ -0,0 +1,44 @@
+//! Poker Math
+//!
+//! # Preliminary Considerations
+//!
+//! - number of starting hands: 169
+//!   - 13 pocket pairs
+//!   - 78 suited hands
+//!   - 78 offsuited hands (excl. pocket pairs)
+//!
+//! - number of possible combinations
+//!   - 52! / (2! * 50!) = 1326 (binominal coefficient)
+//!
+//! # Combinatorics
+//!
+//! - 16 combos of unpaired hands
+//! - 12 combos unpaired offsuit hands
+//! -  4 combos unpaired suited hands
+//! -  6 combos pocket pairs (3 if card is on board)
+//!
+//! ## binominal coefficient
+//!
+//! `k` cards out of `n`: n! / (k! (n-k)!)
+//!
+//!
+//! # Probabilities
+//!
+//!
+//! # Pot Odds
+//!
+//! 2$ into 10$ Pot = 5:1
+//! 5$ into 10$ Pot = 2:1
+//!
+//! # Equity
+//!
+//! Turn:   outs * 4 || outs * 3 + 8 (if outs > 8)
+//! River:  outs * 2 || outs + 8
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
