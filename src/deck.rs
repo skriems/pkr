a41b294b76ab136f8997b0336d7376cf99391e6b
Sebastian Kriems <bastoberto@gmx.de>
2020-06-10 12:39:50 +0200

    moved `get_cards` back into the deck module - one pub fn fewer

--- src/deck.rs
+++ src/deck.rs
@@ -1,6 +1,66 @@
+//! This module holds the `Deck` which either returns itself shuffled
+//! or if it was just baught in the store /shrug
+
 use crate::card::*;
 use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
+
+fn get_cards() -> [Card; 52] {
+    [
+        Card::new(Rank::Ace, Suit::Clubs),
+        Card::new(Rank::King, Suit::Clubs),
+        Card::new(Rank::Queen, Suit::Clubs),
+        Card::new(Rank::Jack, Suit::Clubs),
+        Card::new(Rank::Ten, Suit::Clubs),
+        Card::new(Rank::Nine, Suit::Clubs),
+        Card::new(Rank::Eight, Suit::Clubs),
+        Card::new(Rank::Seven, Suit::Clubs),
+        Card::new(Rank::Six, Suit::Clubs),
+        Card::new(Rank::Five, Suit::Clubs),
+        Card::new(Rank::Four, Suit::Clubs),
+        Card::new(Rank::Three, Suit::Clubs),
+        Card::new(Rank::Two, Suit::Clubs),
+        Card::new(Rank::Ace, Suit::Spades),
+        Card::new(Rank::King, Suit::Spades),
+        Card::new(Rank::Queen, Suit::Spades),
+        Card::new(Rank::Jack, Suit::Spades),
+        Card::new(Rank::Ten, Suit::Spades),
+        Card::new(Rank::Nine, Suit::Spades),
+        Card::new(Rank::Eight, Suit::Spades),
+        Card::new(Rank::Seven, Suit::Spades),
+        Card::new(Rank::Six, Suit::Spades),
+        Card::new(Rank::Five, Suit::Spades),
+        Card::new(Rank::Four, Suit::Spades),
+        Card::new(Rank::Three, Suit::Spades),
+        Card::new(Rank::Two, Suit::Spades),
+        Card::new(Rank::Ace, Suit::Hearts),
+        Card::new(Rank::King, Suit::Hearts),
+        Card::new(Rank::Queen, Suit::Hearts),
+        Card::new(Rank::Jack, Suit::Hearts),
+        Card::new(Rank::Ten, Suit::Hearts),
+        Card::new(Rank::Nine, Suit::Hearts),
+        Card::new(Rank::Eight, Suit::Hearts),
+        Card::new(Rank::Seven, Suit::Hearts),
+        Card::new(Rank::Six, Suit::Hearts),
+        Card::new(Rank::Five, Suit::Hearts),
+        Card::new(Rank::Four, Suit::Hearts),
+        Card::new(Rank::Three, Suit::Hearts),
+        Card::new(Rank::Two, Suit::Hearts),
+        Card::new(Rank::Ace, Suit::Diamonds),
+        Card::new(Rank::King, Suit::Diamonds),
+        Card::new(Rank::Queen, Suit::Diamonds),
+        Card::new(Rank::Jack, Suit::Diamonds),
+        Card::new(Rank::Ten, Suit::Diamonds),
+        Card::new(Rank::Nine, Suit::Diamonds),
+        Card::new(Rank::Eight, Suit::Diamonds),
+        Card::new(Rank::Seven, Suit::Diamonds),
+        Card::new(Rank::Six, Suit::Diamonds),
+        Card::new(Rank::Five, Suit::Diamonds),
+        Card::new(Rank::Four, Suit::Diamonds),
+        Card::new(Rank::Three, Suit::Diamonds),
+        Card::new(Rank::Two, Suit::Diamonds),
+    ]
+}
 
 pub struct Deck {
     pub cards: [Card; 52],
